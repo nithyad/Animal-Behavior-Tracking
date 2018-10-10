e363c70426338314b1eb95e5c194448337f2e0a4
Amy Sorto <amysorto@live.com>
2018-10-09 22:08:37 -0700

    Created a static folder, set it up, and displayed a sample image from the static folder in the template

--- sandiegozoo/settings.py
+++ sandiegozoo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sandiegozoo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'sandiegozoo/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,"static_files")
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "sandiegozoo/static"),
+)
--- sandiegozoo/views.py
+++ sandiegozoo/views.py
@@ -1,17 +1,15 @@
-# from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
-# from django.template import Template, Context
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 def index(request):
-	# # plot active v. non active data on a pie chart
-	# df = pd.DataFrame(3 * np.random.rand(2), index=[100, 600])
-	# df.plot.pie(subplots=True)
-	# plt.savefig('foo.png')
+	# plot active v. non active data on a pie chart
+	df = pd.DataFrame(3 * np.random.rand(2), index=[100, 600])
+	df.plot.pie(subplots=True)
+	plt.savefig('sandiegozoo/static/images/foo.png')
 
-  	t = get_template('sandiegozoo/index.html')
-  	html = t.render({})
-  	return HttpResponse(html)
+	t = get_template('index.html')
+	html = t.render()
+	return HttpResponse(html)
