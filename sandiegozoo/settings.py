b221afe945cee200f446c6085b7107f764ab80e3
Amy Sorto <amysorto@live.com>
2018-10-09 21:08:54 -0700

    Set up template for views.py to use

--- sandiegozoo/settings.py
+++ sandiegozoo/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sandiegozoo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'sandiegozoo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '../Animal-Behavior-Tracking/sandiegozoo/database',
+        'NAME': os.path.join(BASE_DIR, 'sandiegozoo/database'),
     }
 }
 
--- sandiegozoo/views.py
+++ sandiegozoo/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
+# from django.template import Template, Context
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def index(request):
-  return HttpResponse("This is the page for the app view")
+	# # plot active v. non active data on a pie chart
+	# df = pd.DataFrame(3 * np.random.rand(2), index=[100, 600])
+	# df.plot.pie(subplots=True)
+	# plt.savefig('foo.png')
 
+  	t = get_template('sandiegozoo/index.html')
+  	html = t.render({})
+  	return HttpResponse(html)
