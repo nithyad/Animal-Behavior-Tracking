35756512b831637b5a68b7a4e98327ac37e81a16
Amy Sorto <amysorto@live.com>
2018-11-14 12:05:35 -0800

    Added google charts and removed static pie chart

--- sandiegozoo/views.py
+++ sandiegozoo/views.py
@@ -4,11 +4,18 @@ import os.path
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+from django.shortcuts import render
+
 
 def index(request):
-	if (os.path.isfile('sandiegozoo/static/images/activityPieChart.png')):
-		t = get_template('dataDisplay.html')
-	else:
-		t = get_template('noDataDisplay.html') # no found data to display
-	html = t.render()
-	return HttpResponse(html)
+	# Fill in context to pass to 
+	context = {'animal': 'Zebra',
+			   'active': 100,
+			   'nonActive': 240}
+
+	# Render according template
+	if (context == {}): # context is empty, no data
+		return render(request, 'noDataDisplay.html', context)
+	return render(request, 'dataDisplay.html', context)
+
