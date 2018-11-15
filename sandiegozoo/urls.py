b915ba8296f75139c3ea4801e09339bcd5dd4d95
Amy Sorto <amysorto@live.com>
2018-11-14 23:33:16 -0800

    Removed unused polls component of application

--- sandiegozoo/urls.py
+++ sandiegozoo/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', views.index, name='index')
 ]
